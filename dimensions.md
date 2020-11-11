0c206267a81a886e7298b27a0beb7182f033963e
lmarzocchetti <lorenzomarzocchetti1938@gmail.com>
2020-11-11 13:12:08 +0100

    minor changes

---
+++ dimensions.md
@@ -0,0 +1,5 @@
+Altezze(rispetto a 720p):
+	- 30px sopra
+	- 30px sotto
+	- 60px blocchi
+	- 50px(?) personaggi e nemici
--- gamepanel.py
+++ gamepanel.py
@@ -4,6 +4,8 @@ import levelfactory
 prova = pygame.image.load("res/characters/terra_down.png")
 prova = pygame.transform.scale(prova, (prova.get_size()[0] * 2, prova.get_size()[1] * 2))
 sas = pygame.rect.Rect(0, 0, 1280, 720)
+prova1 = pygame.image.load("res/world_1/softblock_0.png")
+prova1 = pygame.transform.scale(prova1, (prova1.get_size()[0] + 44, prova1.get_size()[1] + 44))
 
 
 class Gamepanel:
@@ -24,8 +26,9 @@ class Gamepanel:
     def maincycle(self):
         self.clock.tick(self.fps)
         while self.isRunning:
-            self.screen.fill((255, 255, 255), rect=sas)
+            #self.screen.fill((255, 255, 255), rect=sas)
             self.screen.blit(prova, (self.SCREEN_HEIGHT / 2, self.SCREEN_WIDTH / 2))
+            self.screen.blit(prova1, (self.SCREEN_HEIGHT / 2 - 70, self.SCREEN_WIDTH/2))
 
             pygame.display.update()
 
